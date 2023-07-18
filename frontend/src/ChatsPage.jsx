26b432e0a341bbfc9fc5dfd4f2bac04e0c84a5f5
Shahbazov <ayhanshahbazov@gmail.com>
2023-07-19 01:18:37 +0400

    Front and Back ready

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,15 @@
-const ChatsPage = () => {
-  return <div className="background">chats...</div>;
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div className="background">
+      <PrettyChatWindow
+        projectId="30e6f6d5-d621-443c-880b-24f239a7b01b"
+        username={props.user.username}
+        secret={props.user.secret}
+      />
+    </div>
+  );
 };
 
 export default ChatsPage;
